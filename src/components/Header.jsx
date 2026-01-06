fa8f252e5a4c6cc59ea7a1ac3424d30840c147f8
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-06 15:11:23 +0600

    added initial Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>🤯 StackOverThinking</h1>
+      <p>Your daily dose of useless productivity</p>
+    </header>
+  )
+}
+
+export default Header
